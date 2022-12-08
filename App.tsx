8f478d8181c40c8fe904f5e4f9b65b75af175381
itsaln <itsaln23@gmail.com>
2022-12-09 02:36:30 +0500

    01

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import Header from '@/components/Header'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Header />
+			<Text>Open up App.tsx to start working on your app!</Text>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+})
